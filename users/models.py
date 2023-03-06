ea71fa45e6f4d6a240a9f58012d3c7d26ca1b150
Thaygobrd <tbrduarte@gmail.com>
2023-03-06 10:11:27 -0300

    Initial structure and Requirements

---
+++ users/models.py
@@ -0,0 +1,15 @@
+from django.db import models
+from django.contrib.auth.models import AbstractUser
+
+
+# Create your models here.
+class User(AbstractUser):
+    email = models.EmailField(
+        max_length=127,
+        unique=True,
+        error_messages={"unique": "Email already registered"},
+    )
+    first_name = models.CharField(max_length=127)
+    last_name = models.CharField(max_length=127)
+    is_employee = models.BooleanField(default=False)
+    is_client = models.BooleanField(default=True)
