8c478188309f8d102e8090959d014b2757337322
Lucas <lucassoares2007@hotmail.com>
2023-03-07 14:29:18 -0300

    refactor: products and reviews

--- reviews/views.py
+++ reviews/views.py
@@ -1,17 +1,17 @@
-from rest_framework.generics import CreateAPIView
+from rest_framework.generics import ListCreateAPIView
 from rest_framework_simplejwt.authentication import JWTAuthentication
-from rest_framework.permissions import IsAdminUser
+from rest_framework.permissions import IsAuthenticatedOrReadOnly
 from django.shortcuts import get_object_or_404
 from products.models import Product
 from .models import Review
 from .serializers import ReviewSerializer
 
 
-class ReviewView(CreateAPIView):
+class ReviewView(ListCreateAPIView):
     queryset = Review.objects.all()
     serializer_class = ReviewSerializer
     authentication_classes = [JWTAuthentication]
-    permission_classes = [IsAdminUser]
+    permission_classes = [IsAuthenticatedOrReadOnly]
 
     def perform_create(self, serializer):
         product = get_object_or_404(
