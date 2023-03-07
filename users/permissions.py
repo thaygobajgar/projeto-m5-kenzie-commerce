3f73d1bc62ff8fed35968cfe6371bcc57cb5ef3a
lfeitosaf <lucas-jpl@hotmail.com>
2023-03-07 17:08:00 -0300

    fixed permissions

--- users/permissions.py
+++ users/permissions.py
@@ -5,4 +5,4 @@ from rest_framework.views import View
 
 class IsAdminOrAccountOwner(permissions.BasePermission):
     def has_object_permission(self, request, view: View, obj: User) -> bool:
-        return request.user.is_authenticated and obj == request.user
+        return request.user.is_authenticated and obj == request.user or request.user.is_staff
