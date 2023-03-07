c797cf6608b586f4af37363438c4d21cb4e76405
Lucas <lucassoares2007@hotmail.com>
2023-03-06 23:18:41 -0300

    build: route Products and view Products

---
+++ products/urls.py
@@ -0,0 +1,6 @@
+from django.urls import path
+from .views import ProductView
+
+urlpatterns = [
+    path("products/", ProductView.as_view()),
+]
--- products/views.py
+++ products/views.py
@@ -1,3 +1,15 @@
-from django.shortcuts import render
+from rest_framework.generics import ListCreateAPIView
+from rest_framework_simplejwt.authentication import JWTAuthentication
+from rest_framework.permissions import IsAdminUser
+from .models import Product
+from .serializers import ProductSerializer
 
-# Create your views here.
+
+class ProductView(ListCreateAPIView):
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer
+    authentication_classes = [JWTAuthentication]
+    permission_classes = [IsAdminUser]
+
+    def perform_create(self, serializer):
+        return serializer.save(user=self.request.user)
