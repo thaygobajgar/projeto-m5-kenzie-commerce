7f581f4ff92036e6228dcc3c0dddde1ba994cb65
Rikelly <102254879+rikellyh@users.noreply.github.com>
2023-03-12 01:42:12 -0300

    Revert "Rm/feat/orders"

--- lists/permissions.py
+++
@@ -1,8 +0,0 @@
-from rest_framework import permissions
-from .models import List
-from rest_framework.views import View
-
-
-class IsAccountOwner(permissions.BasePermission):
-    def has_object_permission(self, request, view: View, obj: List) -> bool:
-        return request.user.is_authenticated and obj.user == request.user
